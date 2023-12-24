1b3fa065fa30ce800e676f68d9b796b4b66691ec
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-12-24 15:47:08 -0500

    Added question answer game utilizig two dimensional lists

--- day4/main.py
+++ day4/main.py
@@ -7,7 +7,7 @@ print(a[1:10:2]) #slicing and printing from index 1-10 witha  jump of 2 index
 print(a[-10: -1: 2]) #doing the same thing using resversed index
 
 #List comprehension:
-print([i for i in range(20)], );
+print([i for i in range(20)] )
 
 # list methods
 a.sort();# sorts the list in ascendin order
@@ -18,4 +18,23 @@ a.append(10) # adds element to the last of list
 
 a.reverse() # reverses the list 
 print(a)
-print(a.index(19)) # returns the index of an element in the list if exists otherwise throws an error(-1 is valid index ion pyhton)
+print(a.index(1)) # returns the index of an element in the list if exists otherwise throws an error(-1 is valid index ion pyhton)
+print(a.count(1)) # returns the number of occurances of element
+
+b= a # passed by reference gonna change the original list
+b=a.copy() # copies the list instead of passing by reference
+b.insert(899, 2) # inserts an element at a given index in a list
+
+a.extend(b) # extends a with b means joins ba t the ned of a
+
+#TWo dimensional lists
+points=0
+questionAnswers = [["1+1", "2+3", "3+2", "5+9"], [2,5,5,14]]
+options = questionAnswers[1].copy().sort();
+for i in range(len(questionAnswers[0])):
+    print("What is ", questionAnswers[0][i], "?")
+    answer = int(input("Enter your Answer:"))
+    if(answer==questionAnswers[1][i]):
+        points+=1
+
+print("You got " + str(points) +" out of " +str(len(questionAnswers[0])))
